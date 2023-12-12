63529cda9c858c525123eaff19d47b9eb0d8a613
Mic Hussey <30532590+MichaelHussey@users.noreply.github.com>
2023-12-12 18:55:43 +0100

    Update cflt_cloud.tf
    Added Flink compute pool

--- terraform/cflt_cloud.tf
+++ terraform/cflt_cloud.tf
@@ -47,6 +47,26 @@ resource "confluent_kafka_cluster" "cc_kafka_cluster" {
 }
 
 # --------------------------------------------------------
+# Flink Compute Pool
+# --------------------------------------------------------
+resource "confluent_flink_compute_pool" "cc_flink_compute_pool" {
+  display_name = "${var.cc_dislay_name}-${random_id.id.hex}"
+  cloud        = var.cc_cloud_provider
+  region       = var.cc_cloud_region
+  max_cfu      = var.cc_compute_pool_cfu
+  environment {
+    id = confluent_environment.cc_handson_env.id
+  }
+  depends_on = [
+    confluent_kafka_cluster.cc_kafka_cluster
+  ]
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+
+# --------------------------------------------------------
 # Service Accounts (app_manager, sr, clients)
 # --------------------------------------------------------
 resource "confluent_service_account" "app_manager" {
